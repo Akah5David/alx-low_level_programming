6cfda72684dbb4176bd11af65d82cdfb7c7762fe
Akah5David <davidakah1999@gmail.com>
2023-03-01 04:17:32 -0800

    know

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -3,8 +3,8 @@
 /**
  * main - returns thing at the end of the program
  *
- * description: the funtion prits hello to the world
- * return: return 0 at the end of the program
+ * Description: 'the funtion prits hello to the world'
+ * Return: shows 0 at the end of the program
  */
 
 int main(void)
