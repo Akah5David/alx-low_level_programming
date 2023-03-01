0e657a93ec712bdf7383a384c31ffe17744b1a89
Akah5David <davidakah1999@gmail.com>
2023-03-01 04:06:44 -0800

    know

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - returns thing at the end of the program
+ *
+ * description: the funtion prits hello to the world
+ * return: return 0 at the end of the program
+ */
+
+int main(void)
+{
+printf("hello to the world\n");
+
+return (0);
+}
+
