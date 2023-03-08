0bb50258212bfcfbf0bd1c41831d79b4737b9209
Akah5David <davidakah1999@gmail.com>
2023-03-07 20:54:09 -0800

    far

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -3,12 +3,11 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
- * 
+ *
  * Return: on success 1.
  * On error, -1 is returned, and erro is set appropriately.
  */
 int_putchar(char c)
 {
-return(write(1,&c,1);
+return (write(1, &c, 1));
 }
-
