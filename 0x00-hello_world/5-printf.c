12b0ec2fa4f726b366c03721b2972597512d1527
Akah5David <davidakah1999@gmail.com>
2023-03-18 06:23:45 -0700

    him

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints a statement
+ *
+ * Declaration: it prints a statement on grammer
+ * Return: return 0 always (sucess)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
