41c351f226bbae15df8fc43d3cf31b10c88c5941
Akah5David <davidakah1999@gmail.com>
2023-03-18 00:48:53 -0700

    him

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts - prints a string
+ *
+ * Description: puts print a string that talks about programming
+ * Return: gives 0 always (successful)
+ */
+
+int puts(void)
+{
+putchar ("Programming is like building a multilingual puzzle);
+
+return (0);
+}
---
+++ 0x00-hello_world/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * puts - for printing string
+ *
+ * Return: give 0 always (success)
+ */
+
+int puts(void);
+
+#endif
