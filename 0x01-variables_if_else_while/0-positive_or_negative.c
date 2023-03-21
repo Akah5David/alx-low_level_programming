e2b5a4fff821f3662b0d5caed68abf75fa30c84a
Akah5David <davidakah1999@gmail.com>
2023-03-20 18:55:39 -0700

    hit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("it is positive\n");
+printf("%d is positive\n", n);
 }
-else if (n == 0)
+else if (n < 0)
 {
-printf("it is zero\n");
+printf("%d is negative\n", n);
 }
 else
 {
-printf("it is negative\n");
+printf("%d is zero\n", n);
 }
 return (0);
 }
