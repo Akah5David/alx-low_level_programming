bbc76458832aa6afed4337d6cbf0b25258ea95c6
Akah5David <davidakah1999@gmail.com>
2023-03-22 12:35:39 -0700

    her

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - jkhkjhkjghgghj
+ * @t: jhjhjkjhjhhjjk
+ * @n: ugjkjgjkgkjg
+ *
+ * Return: jkhkjhjkhj
+ */
+
+int add(int t, int n)
+{
+int f;
+f = t + n;
+return (f);
+}
