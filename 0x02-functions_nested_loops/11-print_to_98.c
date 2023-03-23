53352f2721f741986d6f7fa140bd5997f2277cbb
Akah5David <davidakah1999@gmail.com>
2023-03-22 21:18:47 -0700

    her

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - jkhkjgkjgjkh
+ * @n: yfghjhghfghdhdfg
+ *
+ * Return: fghfghgdgv
+ */
+
+void print_to_98(int n)
+{
+int i;
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+
+else
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+
+if (i != 98)
+{
+printf(", ");
+}
+}
+
+}
+printf("\n");
+}
