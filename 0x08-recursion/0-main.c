1fdb8f5dae6a79d699684c5886115b2932aecd8d
Akah5David <davidakah1999@gmail.com>
2023-04-06 20:04:05 -0700

    love

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - assigns a string to the argument variable of calling
+ * funtion
+ *
+ * Return: returns 0 if successful.
+ */
+
+int main(void)
+{
+_puts_recursion("puts with recursion");
+return (0);
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _putchar - print a string and returns an int variable
+ * to the calling function
+ * @c: varaible containing string
+ *
+ * Return: returns 0 if successful.
+ */
+
+int _putchar(int c)
+{
+putchar(c);
+return (0);
+}
