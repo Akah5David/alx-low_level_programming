1085d7c5e6a118de82d2053d19046d9a53558967
Akah5David <davidakah1999@gmail.com>
2023-04-09 14:57:56 -0700

    rome

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument counter and determinant of the size of
+ * argz array.
+ * @argv: vector array or one dimensional array used to
+ * print out the string of arguments(using index values
+ * greater than) and program name(using index value zero)
+ *
+ * Return: returns 0 if the process is true.
+ */
+
+int main(int argc, char *argv[])
+{
+int mul = 1;
+int i;
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+mul *= atoi(argv[i]);
+}
+printf("%d\n", mul);
+}
+else
+{
+printf("Error\n");
+}
+return (0);
+}
