bf09e1010112c3d324518e78451a53c75b512f54
Akah5David <davidakah1999@gmail.com>
2023-04-09 15:13:03 -0700

    push

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 {
 int mul = 1;
 int i;
-if (argc > 1)
+if (argc == 3)
 {
 for (i = 1; i < argc; i++)
 {
@@ -25,7 +25,7 @@ mul *= atoi(argv[i]);
 }
 printf("%d\n", mul);
 }
-else
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
