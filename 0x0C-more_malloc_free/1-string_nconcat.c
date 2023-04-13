4a9778445cc2e792f9cf9ee18ffec86c3b50b6fe
Akah5David <davidakah1999@gmail.com>
2023-04-13 11:17:58 -0700

    love

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * string_nconcat - returns a char at the end of the program
  * @s1: hjkhdsjhj
@@ -18,38 +17,25 @@ unsigned int lens1, lens2;
 /*getting the length of s1 and s2*/
 lens1 = strlen(s1);
 lens2 = strlen(s2);
-
-/*allocating memory for p*/
-
 p = (char *)malloc(lens1 + n + 1);/*assigned first n byte of s2*/
-
 if (p == NULL)
 {
 return (NULL);
 }
-
-if (s1 == NULL && s2 == NULL) /*treating NULL as empty string*/
+if (s1 == NULL) /*treating NULL as empty string*/
 {
 s1 = "";
+}
+if (s2 == NULL) /*treating NULL as empty string*/
+{
 s2 = "";
 }
-
 if (n >= lens2)
 {
 n = lens2;/*using entire length 0f s2 if n >= lens2*/
 }
-
-/*copying s1 into p*/
-strcpy(p, s1);
-
-/*appending the first n byte of s2 into p*/
-
-strncat(p, s2, n);
-/*adding null-terminator to the end of the string*/
-
-p[lens1 + n] = '\0';
-
-
+strcpy(p, s1);/*copying s1 into p*/
+strncat(p, s2, n);/*appending the first n byte of s2 into p*/
+p[lens1 + n] = '\0'; /*adding null-terminator to the end of the string*/
 return (p);
-
 }
