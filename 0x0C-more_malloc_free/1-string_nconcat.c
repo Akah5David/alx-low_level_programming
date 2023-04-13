7b1819a2ca81c1a7a5f9d34439b312e96434c18a
Akah5David <davidakah1999@gmail.com>
2023-04-13 10:39:52 -0700

    love

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - returns a char at the end of the program
+ * @s1: hjkhdsjhj
+ * @s2: dhjdhkjhdjklsdjkljskljd
+ * @n: jkdgkjdghksdajkhdjhjdsh
+ *
+ * Return: returns a character
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *p;
+unsigned int lens1, lens2;
+(void)lens2;
+/*getting the length of s1 and s2*/
+lens1 = strlen(s1);
+lens2 = strlen(s2);
+
+/*allocating memory for p*/
+
+p = (char *)malloc(lens1 + n + 1);/*assigned first n byte of s2*/
+
+if (p == NULL)
+{
+return (NULL);
+}
+
+/*copying s1 into p*/
+strcpy(p, s1);
+
+/*appending the first n byte of s2 into p*/
+
+strncat(p, s2, n);
+/*adding null-terminator to the end of the string*/
+
+p[lens1 + n] = '\0';
+
+return (p);
+
+}
