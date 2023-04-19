179089ba01e0b3adfb44fb160b1f49108201a194
Akah5David <davidakah1999@gmail.com>
2023-04-18 21:44:50 -0700

    love

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,12 @@ char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-typedef struct dog
+/*typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-}dog_t; 
+}dog_t;*/ 
 
 
 #endif
