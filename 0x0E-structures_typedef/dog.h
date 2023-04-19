2b67e100d8bb0b7226094f184f78691f7fd66764
Akah5David <davidakah1999@gmail.com>
2023-04-18 20:40:36 -0700

    dog description

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * dog - dtructure with characteristics.
+ * name: kjlkfkljfd
+ * owner: l;kfglkfl;kl;fkl
+ * age: l;fklgjljfl;dfl
+ *
+ * return: kjkjfkjfkgjkg
+ */
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+#endif
